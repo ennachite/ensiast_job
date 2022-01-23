4f88da75d85ef3c1f69f0f3d3ac9e5efc5dba368
ennachite <oussama_ennachite@um5.ac.ma>
2022-01-23 21:34:07 +0100

    some modifications of DB

--- sql/ensiast_job.sql
+++ sql/ensiast_job.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2022 at 09:20 PM
+-- Generation Time: Jan 23, 2022 at 09:31 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `ensiast_job`
 --
+CREATE DATABASE IF NOT EXISTS `ensiast_job` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ensiast_job`;
 
 -- --------------------------------------------------------
 
@@ -27,19 +29,18 @@ SET time_zone = "+00:00";
 -- Table structure for table `candidacy`
 --
 
-DROP TABLE IF EXISTS `candidacy`;
 CREATE TABLE IF NOT EXISTS `candidacy` (
-  `candidacy_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `offer_id` int(11) NOT NULL,
-  `status` varchar(32) NOT NULL,
-  `CV` varchar(64) NOT NULL,
-  `github_username` varchar(64) DEFAULT NULL,
-  `date_candidacy` varchar(32) NOT NULL,
-  `motivation` text NOT NULL,
-  PRIMARY KEY (`candidacy_id`),
-  KEY `student_id` (`student_id`),
-  KEY `offer_id` (`offer_id`)
+                                           `candidacy_id` int(11) NOT NULL AUTO_INCREMENT,
+                                           `student_id` int(11) NOT NULL,
+                                           `offer_id` int(11) NOT NULL,
+                                           `status` varchar(32) NOT NULL,
+                                           `CV` varchar(64) DEFAULT NULL,
+                                           `github_username` varchar(64) DEFAULT NULL,
+                                           `date_candidacy` varchar(32) NOT NULL,
+                                           `motivation` text DEFAULT NULL,
+                                           PRIMARY KEY (`candidacy_id`),
+                                           KEY `student_id` (`student_id`),
+                                           KEY `offer_id` (`offer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -56,19 +57,18 @@ CREATE TABLE IF NOT EXISTS `candidacy` (
 -- Table structure for table `company`
 --
 
-DROP TABLE IF EXISTS `company`;
 CREATE TABLE IF NOT EXISTS `company` (
-  `company_id` int(11) NOT NULL AUTO_INCREMENT,
-  `member_id` int(11) NOT NULL,
-  `company_name` varchar(64) NOT NULL,
-  `ceo_name` varchar(64) DEFAULT NULL,
-  `company_size` int(5) NOT NULL,
-  `company_fix` varchar(16) DEFAULT NULL,
-  `company_tif` varchar(32) NOT NULL,
-  `founded` int(4) NOT NULL,
-  `description` text DEFAULT NULL,
-  PRIMARY KEY (`company_id`),
-  KEY `member_id` (`member_id`)
+                                         `company_id` int(11) NOT NULL AUTO_INCREMENT,
+                                         `member_id` int(11) NOT NULL,
+                                         `company_name` varchar(64) NOT NULL,
+                                         `ceo_name` varchar(64) DEFAULT NULL,
+                                         `company_size` int(5) NOT NULL,
+                                         `company_fix` varchar(16) DEFAULT NULL,
+                                         `company_tif` varchar(32) NOT NULL,
+                                         `founded` int(4) NOT NULL,
+                                         `description` text DEFAULT NULL,
+                                         PRIMARY KEY (`company_id`),
+                                         KEY `member_id` (`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -83,16 +83,15 @@ CREATE TABLE IF NOT EXISTS `company` (
 -- Table structure for table `member`
 --
 
-DROP TABLE IF EXISTS `member`;
 CREATE TABLE IF NOT EXISTS `member` (
-  `member_id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `role` varchar(10) NOT NULL,
-  `city` varchar(64) DEFAULT NULL,
-  `picture` varchar(255) DEFAULT NULL,
-  `inscription_date` varchar(64) NOT NULL,
-  PRIMARY KEY (`member_id`)
+                                        `member_id` int(11) NOT NULL AUTO_INCREMENT,
+                                        `email` varchar(64) NOT NULL,
+                                        `password` varchar(64) NOT NULL,
+                                        `role` varchar(10) NOT NULL,
+                                        `city` varchar(64) DEFAULT NULL,
+                                        `picture` varchar(255) DEFAULT NULL,
+                                        `inscription_date` varchar(64) NOT NULL,
+                                        PRIMARY KEY (`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -105,19 +104,18 @@ CREATE TABLE IF NOT EXISTS `member` (
 -- Table structure for table `offer`
 --
 
-DROP TABLE IF EXISTS `offer`;
 CREATE TABLE IF NOT EXISTS `offer` (
-  `offer_id` int(11) NOT NULL AUTO_INCREMENT,
-  `company_id` int(11) NOT NULL,
-  `offer_name` varchar(64) NOT NULL,
-  `offer_type` varchar(16) NOT NULL,
-  `offer_salary` int(6) DEFAULT NULL,
-  `offer_location` varchar(64) NOT NULL,
-  `offer_domain` varchar(64) NOT NULL,
-  `job_type` varchar(64) DEFAULT NULL,
-  `offer_description` text NOT NULL,
-  PRIMARY KEY (`offer_id`),
-  KEY `company_id` (`company_id`)
+                                       `offer_id` int(11) NOT NULL AUTO_INCREMENT,
+                                       `company_id` int(11) NOT NULL,
+                                       `offer_name` varchar(64) NOT NULL,
+                                       `offer_salary` int(6) DEFAULT NULL,
+                                       `offer_location` varchar(64) NOT NULL,
+                                       `offer_domain` varchar(64) NOT NULL,
+                                       `job_type` varchar(64) DEFAULT NULL,
+                                       `offer_description` text NOT NULL,
+                                       `post_time` varchar(60) NOT NULL,
+                                       PRIMARY KEY (`offer_id`),
+                                       KEY `company_id` (`company_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -132,14 +130,13 @@ CREATE TABLE IF NOT EXISTS `offer` (
 -- Table structure for table `profile`
 --
 
-DROP TABLE IF EXISTS `profile`;
 CREATE TABLE IF NOT EXISTS `profile` (
-  `profile_id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` int(11) NOT NULL,
-  `active` varchar(10) NOT NULL,
-  `summary` text DEFAULT NULL,
-  PRIMARY KEY (`profile_id`),
-  KEY `student_id` (`student_id`)
+                                         `profile_id` int(11) NOT NULL AUTO_INCREMENT,
+                                         `student_id` int(11) NOT NULL,
+                                         `active` varchar(10) NOT NULL,
+                                         `summary` text DEFAULT NULL,
+                                         PRIMARY KEY (`profile_id`),
+                                         KEY `student_id` (`student_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -154,22 +151,21 @@ CREATE TABLE IF NOT EXISTS `profile` (
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
 CREATE TABLE IF NOT EXISTS `student` (
-  `student_id` int(11) NOT NULL AUTO_INCREMENT,
-  `member_id` int(11) NOT NULL,
-  `first_name` varchar(64) NOT NULL,
-  `last_name` varchar(64) NOT NULL,
-  `cne` varchar(32) NOT NULL,
-  `cin` varchar(32) NOT NULL,
-  `birthdate` varchar(32) NOT NULL,
-  `gender` varchar(10) NOT NULL,
-  `specialty` varchar(32) NOT NULL,
-  `promo` int(4) NOT NULL,
-  `year_studies` varchar(10) NOT NULL,
-  `phone` varchar(16) NOT NULL,
-  PRIMARY KEY (`student_id`),
-  KEY `member_id` (`member_id`)
+                                         `student_id` int(11) NOT NULL AUTO_INCREMENT,
+                                         `member_id` int(11) NOT NULL,
+                                         `first_name` varchar(64) NOT NULL,
+                                         `last_name` varchar(64) NOT NULL,
+                                         `cne` varchar(32) NOT NULL,
+                                         `cin` varchar(32) NOT NULL,
+                                         `birthdate` varchar(32) NOT NULL,
+                                         `gender` varchar(10) NOT NULL,
+                                         `specialty` varchar(32) NOT NULL,
+                                         `promo` int(4) NOT NULL,
+                                         `year_studies` varchar(10) NOT NULL,
+                                         `phone` varchar(16) NOT NULL,
+                                         PRIMARY KEY (`student_id`),
+                                         KEY `member_id` (`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -186,32 +182,32 @@ CREATE TABLE IF NOT EXISTS `student` (
 -- Constraints for table `candidacy`
 --
 ALTER TABLE `candidacy`
-  ADD CONSTRAINT `candidacy_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
-  ADD CONSTRAINT `candidacy_ibfk_2` FOREIGN KEY (`offer_id`) REFERENCES `offer` (`offer_id`);
+    ADD CONSTRAINT `candidacy_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
+    ADD CONSTRAINT `candidacy_ibfk_2` FOREIGN KEY (`offer_id`) REFERENCES `offer` (`offer_id`);
 
 --
 -- Constraints for table `company`
 --
 ALTER TABLE `company`
-  ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
+    ADD CONSTRAINT `company_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
 
 --
 -- Constraints for table `offer`
 --
 ALTER TABLE `offer`
-  ADD CONSTRAINT `offer_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`);
+    ADD CONSTRAINT `offer_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`);
 
 --
 -- Constraints for table `profile`
 --
 ALTER TABLE `profile`
-  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
+    ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
 
 --
 -- Constraints for table `student`
 --
 ALTER TABLE `student`
-  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
+    ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
